573d71c280ba5df9a70fa6d397d398195174a346
ThatOneRetard <jupiterjokeyou@gmail.com>
2020-06-03 21:12:52 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Party-Death-Markers
+# party-death-markers
+Spawns large beacons on top of dead party members.
+
+## Info
+- Tanks = red. Healers = Teal. DPS = Blue.
+- The item cannot be picked up. It's fake. Spawned client-side for your eyes only. No body else can see them.
+
+
+- Forked from Teralove 
+---
+
+![Screenshot](https://i.imgur.com/NVEizyL.png)
---
+++ index.js
@@ -0,0 +1,84 @@
+module.exports = function PartyDeathMarkers(mod) {
+  let members = []
+  let markers = []
+
+  mod.game.on('enter_game', removeAllMarkers)
+
+  mod.hook('S_PARTY_MEMBER_LIST', 7, event => {
+    members = event.members
+  })
+
+  mod.hook('S_DEAD_LOCATION', 2, event => {
+    spawnMarker(members.find(member => member.gameId === event.gameId), event.loc)
+  })
+
+  mod.hook('S_SPAWN_USER', 15, event => {
+    if (!event.alive)
+      spawnMarker(members.find(member => member.gameId === event.gameId), event.loc)
+  })
+
+  mod.hook('S_PARTY_MEMBER_STAT_UPDATE', 3, event => {
+    if (mod.game.me.playerId === event.playerId)
+      return
+
+    if (markers.length > 0 && event.alive && event.curHp > 0)
+      removeMarker(members.find(member => member.playerId === event.playerId))
+  })
+
+  mod.hook('S_LEAVE_PARTY_MEMBER', 2, event => {
+    removeMarker(members.find(member => member.playerId === event.playerId))
+  })
+
+  mod.hook('S_LEAVE_PARTY', 1, () => {
+    removeAllMarkers()
+    members = []
+  })
+
+  function spawnMarker(member, loc) {
+    if (!member || mod.game.me.is(member.gameId))
+      return
+
+    removeMarker(member)
+    markers.push(member.playerId)
+
+    mod.toClient('S_SPAWN_DROPITEM', 8, {
+      gameId: member.playerId,
+      loc: loc,
+      item: getMarker(member.class),
+      amount: 1,
+      expiry: 999999,
+      owners: [{playerId: mod.game.me.playerId}]
+    })
+  }
+
+  function getMarker(classid) {
+    switch (classid) {
+      case 1:
+      case 10:
+        return 89616
+      case 6:
+      case 7:
+        return 89614
+      default:
+        return 89615
+    }
+  }
+
+  function removeMarker(member) {
+    if (!member)
+      return
+
+    const id = member.playerId
+    if (markers.includes(id)) {
+      mod.toClient('S_DESPAWN_DROPITEM', 4, {
+        gameId: id
+      })
+      markers = markers.filter(marker => marker !== id)
+    }
+  }
+
+  function removeAllMarkers() {
+    members.forEach(member => removeMarker(member))
+    markers = []
+  }
+}
